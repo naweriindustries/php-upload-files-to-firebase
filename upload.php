380ea2daa92fb885fe3307003f118101c1e9ebec
Octon Labs <130235251+naweriyowericlinton@users.noreply.github.com>
2023-12-03 10:31:24 +0300

    Create upload.php
    make sure you read the firebase documentation before use at https://firebase.google.com/docs

---
+++ upload.php
@@ -0,0 +1,38 @@
+<?php
+$firebaseConfig = [
+    'project_id' => 'your_project_id',
+    'api_key' => 'your_api_key',
+    // Add other configuration details here
+];
+
+$filePath = '/path/to/your/file.jpg'; // Replace this with the path to your file
+$fileName = 'uploaded_file.jpg'; // Name for the file in Firebase Storage
+
+// Firebase Storage URL
+$storageUrl = "https://firebasestorage.googleapis.com/v0/b/{$firebaseConfig['project_id']}.appspot.com/o/{$fileName}?uploadType=media";
+
+$fileContent = file_get_contents($filePath);
+
+$curl = curl_init();
+curl_setopt_array($curl, [
+    CURLOPT_URL => $storageUrl,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => $fileContent,
+    CURLOPT_HTTPHEADER => [
+        'Content-Type: application/octet-stream',
+        'Authorization: Bearer ' . $firebaseConfig['api_key'],
+    ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error: " . $err;
+} else {
+    echo "File uploaded successfully!";
+}
+?>
